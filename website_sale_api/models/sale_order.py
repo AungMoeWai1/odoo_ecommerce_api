29e009093dd73ea4e425fe72972a661e89c33da7
AungMoeWai1 <tkyawwai678@gmail.com>
2026-08-19 13:36:26 +0630

    [FIX] website_sale_api: fix not to search default value shipping status for order.

--- website_sale_api/models/sale_order.py
+++ website_sale_api/models/sale_order.py
@@ -1,7 +1,7 @@
 """Inherit Sale order to add shipping status"""
 
 # pylint:disable=import-error,too-few-public-methods,protected-access
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -12,19 +12,20 @@ class SaleOrder(models.Model):
     shipping_status_id = fields.Many2one(
         "shipping.status",
         string="Shipping Status",
-        default=lambda self: self._get_default_shipping_status(),
     )
 
-    def _get_default_shipping_status(self):
-        """Get default shipping status (Draft)"""
-        if not self.website_id:
-            return self.env.ref("website_sale_api.shipping_status_draft").id
-        return False
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('website_id'):
+                vals['shipping_status_id'] = self.env.ref("website_sale_api.shipping_status_draft"
+                                                          ).id
+        return super().create(vals_list)
 
     def action_confirm(self):
         """Action confirm the order"""
         if self.website_id:
-            self.shipping_status_id = self.env.ref(
+            self.write({"shipping_status_id": self.env.ref(
                 "website_sale_api.shipping_status_order_confirmed"
-            ).id
+            ).id})
         return super().action_confirm()
